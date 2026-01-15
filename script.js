edb47c6e6b325e932a882e7772c890bb2e785a44
Aethdae <103084018+Aethdae@users.noreply.github.com>
2026-01-14 20:36:22 -0600

    Added button to show image, image shows at random spot, working on backend.

--- script.js
+++ script.js
@@ -6,30 +6,90 @@ const gameContainer = doc.getElementById("gameContainer");
 const ctx = gameZone.getContext("2d");
 const tilePadding = 4;
 const tileSize = 12;
-const buttons = [];
 const gameUpdateTimerMS = 5000;
+const images = ["x.png", "x.png", "x.png", "x.png"];
+const promises = [];
+const buttonRectOne = [100, 160, 300, 330];
 
 async function startUp()
 {
+    console.log("Images loading");
     await loadImages();
+    console.log("Images done!");
     await loadJson();
-    year.innerHTML = getYear.valueOf("year");
+    year.innerHTML = getYear;
+    createRect(buttonRectOne[0], buttonRectOne[2], buttonRectOne[1] - buttonRectOne[0], buttonRectOne[3] - buttonRectOne[2], true);
     gameZone.addEventListener("click", function(event)
     {
         const mousePosition = getClickLocation(gameZone, event);
         console.log(`${mousePosition.x}, ${mousePosition.y}`);
-        createImg(mousePosition.x, mousePosition.y);
+        if (clickInBoxOne(mousePosition))
+        {
+            createImg(Math.random() * 400, Math.random() * 200);
+        }
     });
 }
 
+function clickInBoxOne(mousePosition)
+{
+    if (mousePosition.x >= buttonRectOne[0] && mousePosition.x <= buttonRectOne[1] &&
+        mousePosition.y >= buttonRectOne[2] && mousePosition.y <= buttonRectOne[3])
+        {
+            return true;
+        }
+    return false;
+}
 async function loadImages()
 {
-    await Promise.all(
-        Array.from(doc.images).map(
-            (image) =>
-                new Promise((resolve) => image.addEventListener("load"), resolve)
-        )
-    )
+    // await Promise.all(
+    //     Array.from(images).map(
+    //         (image) => new Promise((resolve) => image.addEventListener("load"), resolve)
+    //     )
+    // )
+    // await Promise.all(
+    //     Array.from(images).map(
+    //         (image) => new Promise((resolve) => image.addEventListener("load"), resolve)
+    //         .then(loaded)
+    //     )
+    // )
+    // const promise = new Promise((resolve, reject) => {
+    //     setTimeout(() => {
+    //         resolve("loaded");
+    //     }, 1000);
+    // });
+    // promise.then(loaded, failed);
+
+    images.forEach((image) => promises.push(loadImage(image)));
+
+
+    try {
+        //const xImage = await loadImage("x.png");
+    }
+    catch (error) {
+        //console.log("Yo I failed images");
+    }
+}
+
+function loadImage(imageLocation)
+{
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => {
+            resolve(img); 
+            console.log("I have resolved");
+        }
+        img.onerror = reject;
+        img.src = imageLocation;
+    });
+}
+
+function loaded()
+{
+    console.log("LASDFKASDF");
+}
+function failed()
+{
+    console.log("FAILED");
 }
 
 async function loadJson()
@@ -68,10 +128,9 @@ function createTiles()
         for (let x = tilePadding; x < gameZone.getAttribute("width") - tilePadding; x += tileSize + tilePadding)
         {
                 createImg(x, y);
-                //createRect(x, y, tileSize, tileSize, true, "red");
                 if (Math.random() < .3)
                 {
-                    createImg(x, y);
+                    //createImg(x, y);
                 }
         }
     }
@@ -96,6 +155,10 @@ function createRect(x, y, w, h, filled = false, color = "black")
         ctx.fill();
     }
     ctx.closePath();
+
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+    ctx.fillText("Button", x + w / 2, y + h / 2);
 }
 
 startUp();
